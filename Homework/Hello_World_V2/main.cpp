c22befc5e2b4e6671ccb75934690b90b1e39965b
NKanabolo <nornkable@yahoo.com>
2014-06-24 15:08:39 -0700

    Initial commit
    Signed-off-by: NKanabolo <nornkable@yahoo.com>

---
+++ Homework/Hello_World_V2/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: Nornubari Kanabolo
+ * Purpose: Homework
+ * Created on June 24, 2014, 2:37 PM
+ */
+
+#include <iostream>//User Defined Libraries
+using namespace std;
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+
+int main(int argc, char** argv) {
+//Output a String
+    cout<<"Hello World"<<endl;
+    return 0;
+}
+
