eda5818696a32ed4b5c9eaa41fcd09ce0650a3ca
NKanabolo <nornkable@yahoo.com>
2014-07-06 15:19:27 -0700

    no message
    Signed-off-by: NKanabolo <nornkable@yahoo.com>

---
+++ Homework/Assignment 3/Savitch_8thE_Chap3_Prob1/main.cpp
@@ -0,0 +1,135 @@
+/* 
+ * File:   main.cpp
+ * Author: Nornubari Kanabolo
+ * Savitch 8th EDition Chapter 3 Problem 1
+ * Rock, Paper, Scissors Game
+ * Created on July 6, 2014, 1:26 PM
+ */
+
+#include <iostream>//User Defined Libraries
+
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution
+
+int main(int argc, char** argv) {
+    cout<<"This is a game of Paper, Rock, Scissors\n";
+    cout<<"\n";
+    
+    //Declare Variables
+    string PAPER_WINS = "Paper covers rock, ";
+    string ROCK_WINS = "Rock breaks scissors, ";
+    string SCISSOR_WINS = "Scissors cuts paper, ";
+    string PLAYER_ONE_WINS = "player one wins!\n";
+    string PLAYER_TWO_WINS = "player two wins!\n";
+    string NOBODY_WINS = "Nobody wins.\n";
+    string INVALID_LTTR_P1 = "Invalid letter entry for Player 1!\n";
+    string INVALID_LTTR_P2 = "Invalid letter entry for Player 2!\n";
+    
+    //Inputs
+    char player_one, player_two;
+    char over;  
+    
+    //Game Loop
+    bool running=true;
+    while (running) {
+                    //Get user input
+                    cout<<"Enter P for paper, R for rock, or S for scissors.\n";
+                    cout<<"Player 1 entry: ";
+                    cin>>player_one;
+                    cout<<"Player 2 entry: ";
+                    cin>>player_two;
+                    cout<<endl;
+        //Who wins and how the game works                 
+        switch (player_one) {
+                        case 'R':
+                        case 'r':
+                            switch (player_two) {
+                                case 'R':   
+                                case 'r':
+                                    cout<<NOBODY_WINS;
+                                    break;
+                                case 'P':
+                                case 'p':
+                                    cout<<(PAPER_WINS + PLAYER_TWO_WINS);
+                                    break;
+                                case 'S':
+                                case 's':
+                                    cout<<(ROCK_WINS + PLAYER_ONE_WINS);
+                                    break;
+                                default:
+                                    cout<<INVALID_LTTR_P2;
+                            }
+                            break;
+                        case 'P':
+                        case 'p':
+                            switch (player_two) {
+                                case 'R':
+                                case 'r':
+                                    cout<<(PAPER_WINS + PLAYER_ONE_WINS);
+                                    break;
+                                case 'P':
+                                case 'p':
+                                    cout<<NOBODY_WINS;
+                                    break;
+                                case 'S':
+                                case 's':
+                                    cout<<(SCISSOR_WINS + PLAYER_TWO_WINS);
+                                    break;
+                                default:
+                                    cout<<INVALID_LTTR_P2;
+                            }
+                            break;
+                        case 'S':
+                        case 's':
+                            switch (player_two) {
+                                case 'R':
+                                case 'r':
+                                    cout<<(ROCK_WINS + PLAYER_TWO_WINS);
+                                    break;
+                                case 'P':
+                                case 'p':
+                                    cout<<(SCISSOR_WINS + PLAYER_ONE_WINS);
+                                    break;
+                                case 'S':
+                                case 's':
+                                    cout<<NOBODY_WINS;
+                                    break;
+                                default:
+                                    cout<<INVALID_LTTR_P2;
+                            }
+                            break;
+                        default:
+                            cout<<INVALID_LTTR_P1;
+                    }
+    
+    bool valid = false; //Assume a non-valid answer
+                    while (!valid) {
+                        //Get input from user
+                        cout<<endl;
+                        cout<<"Want to play again?[Y/N]: ";
+                        cin>>over;
+                        //Check to see if the input is valid
+                        switch (over) {
+                            case 'Y':
+                            case 'y':
+                                valid = true;
+                                break;
+                            case 'N':
+                            case 'n':
+                                cout<<"Until we meet again.\n";
+                                valid = true;
+                                running = false;
+                                break;
+                            default:
+                                cout<<"That is not a valid answer.\n";
+                        }
+                    }
+    }
+        return 0;
+}
+
