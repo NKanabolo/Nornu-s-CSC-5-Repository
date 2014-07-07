1696c2caa9bfefb80c93d643180178c757d97d22
NKanabolo <nornkable@yahoo.com>
2014-07-07 15:24:38 -0700

    no message
    Signed-off-by: NKanabolo <nornkable@yahoo.com>

---
+++ Homework/Assignment 3/Gaddis_7thED_Chap4_Prob7/main.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   main.cpp
+ * Author: Nornubari's Laptop
+ *
+ * Created on July 7, 2014, 2:16 PM
+ */
+
+//User Defined Libraries
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution
+
+int main(int argc, char** argv) {
+    //Declare variables
+    const int SECOND_MIN = 60;
+    const int SECOND_HR = 3600;
+    const int SECOND_DAY = 86400;
+    //Input
+    int seconds;
+    cout<<"Enter a number in seconds: ";
+    cin>>seconds;
+    cout<<endl;
+    //Output
+    cout<<fixed;
+    if (seconds >= SECOND_DAY)
+        cout<<"There are "<<(seconds/SECOND_DAY)<<" days ";
+    else if (seconds >= SECOND_HR)
+        cout<<"There are "<<(seconds/SECOND_HR)<<" hours ";
+    else if (seconds >= SECOND_MIN)
+        cout<<"There are "<<(seconds/SECOND_MIN)<<" minutes ";
+    return 0;
+}
+
