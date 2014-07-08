a179f152ec3e3072ceab3998dd48604b7c83c0da
NKanabolo <nornkable@yahoo.com>
2014-07-07 18:09:52 -0700

    Signed-off-by: NKanabolo <nornkable@yahoo.com>

--- Homework/Assignment 3/Gaddis_7thED_Chap4_Prob18/main.cpp
+++ Homework/Assignment 3/Gaddis_7thED_Chap4_Prob18/main.cpp
@@ -5,7 +5,7 @@
  * Created on July 7, 2014, 3:26 PM
  */
 
-#include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
@@ -14,29 +14,26 @@ using namespace std;
  */
 int main(int argc, char** argv) {
     //Declare variables
-    const float GAM_UB = 1e-11;//Upper bound on Gamma Rays
-    const float X_UB = 1e-8;//Upper bound on X Rays
-    const float UV_UB = 4e-7;//Upper bound on Ultraviolet radiation
-    const float VL_UB = 7e-7;//Upper bound on Visible Light
-    const float IR_UB = 1e-3;//Upper bound on Infrared radiation
-    const float MW_UB = 1e-2;//Upper bound on Microwaves
-    //Inputs
-    float wav_len;//Wavelength given by user
+    const float gam_upb = 1e-11;
+    const float xray_upb = 1e-8;
+    const float uv_upb = 4e-7;
+    const float vis_upb = 7e-7;
+    const float infr_upb = 1e-3;
+    const float mw_upb = 1e-2;
+    //Input Wavelength
+    float wavelength;
+    cout<<"Please enter a wavelength(in meters): ";
+    cin>>wavelength;
+    cout<<endl;
     //Outputs
     string rad_type;//Radiation type
-    //Get wavelength from user
-    cout<<"Please enter a wavelength(in meters): ";
-    cin>>wav_len;
-    cout<<endl;
-    //Determine what type of radiation the given wavelength is
-    rad_type=(wav_len<=GAM_UB)?"Gamma Rays":
-             (wav_len<=X_UB)?"X Rays":
-             (wav_len<=UV_UB)?"Ultraviolet":
-             (wav_len<=VL_UB)?"Visible Light":
-             (wav_len<=IR_UB)?"Infrared":
-             (wav_len<=MW_UB)?"Microwaves":"Radio Waves";
-    //Output the radiation type to the screen
-    cout<<"The type of radiation is: "<<rad_type<<endl;
+    rad_type=(wavelength<=gam_upb)?"Gamma Rays":
+             (wavelength<=xray_upb)?"X Rays":
+             (wavelength<=uv_upb)?"Ultraviolet":
+             (wavelength<=vis_upb)?"Visible Light":
+             (wavelength<=infr_upb)?"Infrared":
+             (wavelength<=mw_upb)?"Microwaves":"Radio Waves";
+    cout<<"The type of radiation is for wavelength "<<wavelength<<"meters is: "<<rad_type<<endl;
     return 0;
 }
 
