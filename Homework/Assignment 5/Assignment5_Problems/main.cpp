6864e280e5da4a6bbe617e0db56fb00b1805bc5b
NKanabolo <nornkable@yahoo.com>
2014-07-23 18:12:52 -0700

    Signed-off-by: NKanabolo <nornkable@yahoo.com>

--- Homework/Assignment 5/Assignment5_Problems/main.cpp
+++ Homework/Assignment 5/Assignment5_Problems/main.cpp
@@ -41,9 +41,9 @@ void convIn(int& ft, int& in);
 void convOut(int ft, int in, int mtr, int ctm);
 void fttoMtrCm(int ft, int in, int& mtr, int& ctm);
 //Used in problem 6
-float perim(float sde_1, float sde_2, float sde_3);
+float perime(float sde_1, float sde_2, float sde_3);
 bool validLength(float sde_1, float sde_2, float sde_3);
-void areaperim(float sde_1, float sde_2, float sde_3, float& perim, float& area);
+void areaperim(float sde_1, float sde_2, float sde_3, float& perime, float& area);
 
 //Execution
 int main(int argc, char** argv) {
@@ -170,7 +170,7 @@ int main(int argc, char** argv) {
                 //Inputs
                 float s1, s2, s3, s4;//Input for average and standard deviation
                 //Outputs
-                float average;//Average of the 4 inputs
+                float avrg;//Average of the 4 inputs
                 float stand_devi;//Standard deviation of the four inputs
                 
                 //Program loop
@@ -185,10 +185,10 @@ int main(int argc, char** argv) {
                     cout<<endl;
                     
                     //Calculate average and standard deviation
-                    avgStandDevi(s1, s2, s3, s4, average, stand_devi);
+                    avgStandDevi(s1, s2, s3, s4, avrg, stand_devi);
                     
                     //Output answer
-                    cout<<"Average of the 4 numbers is: "<<average<<endl;
+                    cout<<"Average of the 4 numbers is: "<<avrg<<endl;
                     cout<<"The standard deviation is: "<<stand_devi<<endl<<endl;
                     
                 }while(repeat());
@@ -301,16 +301,8 @@ bool repeat(){
     
     return (answer == 'Y' || answer == 'y');
 }
-//Problem 1 functions
-
+//Problem 1 and 2 functions
 //Function that allows the user to input military time
-//Output call by reference
-//Input
-//  hr = hour
-//  mn = minute
-//Output
-//  hr = hour
-//  mn = minute
 void gettime(int& hr, int& mn){
     //Inputs from user
     cout<<"Enter time [hour minute]: ";
@@ -318,81 +310,40 @@ void gettime(int& hr, int& mn){
     cin>>mn;
 }
 //Correctly prints military time
-//Inputs
-//  time = some value of time in hours, minutes, or seconds
 void miltaout(int time){
     if(time<10)
         cout<<"0";
     cout<<time;
 }
-//Problem 2 functions
-void gettime(int& hr, int& mn){
-    //Inputs from user
-    cout<<"Enter time [hour minute]: ";
-    cin>>hr;
-    cin>>mn;
-}
 //Problem 3 functions
-
 //Calculates the average of four numbers
-//Inputs
-//  s1, s2, s3, s4 = four numbers
-//Outputs
-//  the average value of s1, s2, s3, s4
 float average(float s1, float s2, float s3, float s4){
     //Calculate and return the average
     return (s1+s2+s3+s4)/4;
 }
 //Calculates one standard deviation term
-//Inputs
-//  s = a single number
-//  average = the average of four values
-//Outputs
-//  the standard deviation of s1, s2, s3, s4
-float stddevi(float s, float average){
+float stddevi(float s, float avrg){
     //Calculate and return one term of the standard deviation
-    return (s-average)*(s-average);
+    return (s-avrg)*(s-avrg);
 }
 //Calculate the average and standard deviation of four numbers
-//Outputs by reference
-//Inputs
-//  s1, s2, s3, s4 = four numbers
-//Outputs
-//  average = average of s1, s2, s3, s4
-//  stand_devi = standard deviation of s1, s2, s3, s4
-void avgStandDevi(float s1, float s2, float s3, float s4, float& average, float& stand_devi){
+void avgStandDevi(float s1, float s2, float s3, float s4, float& avrg, float& stand_devi){
     //Calculate average
-    average = average(s1, s2, s3, s4);
+    avrg = average(s1, s2, s3, s4);
     //Calculate standard deviation
-    stand_devi = sqrt(average(stddevi(s1, average), stddevi(s2, average),
-                           stddevi(s3, average), stddevi(s4, average)));
+    stand_devi = sqrt(average(stddevi(s1, avrg), stddevi(s2, avrg),
+                           stddevi(s3, avrg), stddevi(s4, avrg)));
 }
 //Problem 4 functions
-
 //Computes the number of coins of a certain denomination that can be fit into
-//a given amount of change
-//Output by reference
-//Inputs
-    //coin_value = The value of a type of coin
-    //num = The number of coins of coin_value that can fit into amount_left
-    //amount_left = The amount of change left
-//Outputs
-    //num
-    //amount_left
 void calc_coins(int coin_val, int& numb, int& amount_rem){
     //Calculate num and amount_left
     numb = amount_rem/coin_val;
     amount_rem %= coin_val;
 }
 //Problem 5 functions
-
 //Gets an input of feet and inches from the user
-//Output by reference
-//No Inputs
-//Outputs
-//  ft = feet
-//  in = inches
-void conv4In(int& ft, int& in){
+void convIn(int& ft, int& in){
     //Get input from user
     cout<<"Enter length in feet and inches\n";
     cout<<"Length [ft in]: ";
@@ -401,24 +352,11 @@ void conv4In(int& ft, int& in){
     cout<<endl;
 }
 //Outputs how many meters and centimeters are equivalent to feet and inches
-//No outputs
-//Inputs
-//  ft = feet
-//  in = inches
-//  m  = meters
-//  cm = centimeters
 void convOut(int ft, int in, int mtr, int ctm){
     cout<<ft<<" feet and "<<in<<" inches is equivalent to: ";
-    cout<<mtr<<" meters and "<<cm<<" centimeters\n\n";
+    cout<<mtr<<" meters and "<<ctm<<" centimeters\n\n";
 }
 //Calculate the meters and centimeters given feet and inches
-//Output by reference
-//Inputs
-//  ft = feet
-//  in = inches
-//Outputs
-// m  = meters
-// cm = centimeters
 void fttoMtrCm(int ft, int inches, int& mtr, int& ctm){
     //Define variables
     float ft_float = ft;//Feet as a float
@@ -433,45 +371,27 @@ void fttoMtrCm(int ft, int inches, int& mtr, int& ctm){
     
 }
 //Problem 5 functions
-
 //Calculate the semi-perimeter of a triangle given the lengths of 3 sides
-//Inputs
-//  sd_1, sd_2, sd_3 = sides of the triangle
-//Outputs
-//  s_prtr = semi-perimeter
-float perim(float sde_1, float sde_2, float sde_3){
+float perime(float sde_1, float sde_2, float sde_3){
     //Calculate and return the semi-perimeter
     return (sde_1+sde_2+sde_3)/2;
 }
 //Determines if three lengths can form a triangle
-//Formula: Three positive lengths of side a, b, and c, form a triangle iff
-//a+b>c, a+c>b, and b+c>a
-//Inputs
-//  sd_1, sd_2, sd_3 = lengths of three sides
-//Outputs
-//  bool determining if the lengths can form a triangle
+//this bool determines if the lengths can form a triangle
 bool validLength(float sde_1, float sde_2, float sde_3){
     return (sde_1>0 && sde_2>0 && sde_3>0 &&
             (sde_1+sde_2)>sde_3 && (sde_1+sde_3)>sde_2 && (sde_2+sde_3)>sde_1);
 }
 //Calculate the area and perimeter of a triangle given the lengths of 3 sides
-//For invalid data (the lengths can't form a triangle), return -1 for both
-//the area and perimeter.
-//Output by reference
-//Inputs
-//  sd_1, sd_2, sd_3 = sides of the triangle
-//Outputs
-//  area = area
-//  prtr = perimeter
 void areaperim(float sde_1, float sde_2, float sde_3, float& perim, float& area){
     //Declare variables
-    float sd_perim;//Semi-perimeter
+    float sd_perime;//Semi-perimeter
     
     //Check that the input is valid and calculate results
-    if(validLength(sd_1, sd_2, sd_3)){
-        sd_perim= perim(sd_1, sd_2, sd_3);
+    if(validLength(sde_1, sde_2, sde_3)){
+        sd_perime= perime(sde_1, sde_2, sde_3);
         perim = sde_1+sde_2+sde_3;
-        area = sqrt(sd_perim*(sd_perim-sde_1)*(sd_perim-sde_2)*(sd_perim-sde_3));
+        area = sqrt(sd_perime*(sd_perime-sde_1)*(sd_perime-sde_2)*(sd_perime-sde_3));
     }
     else
         perim = area = -1;
